8e35e20137c3e96cffe4531f1af914c9c9286bb3
huylc12343 <huylc12343@gmail.com>
2024-10-26 10:47:31 +0700

    fix cant play video problem

--- assets/js/lsqt.js
+++ assets/js/lsqt.js
@@ -5,20 +5,20 @@ function showCategory(category) {
     let images = [];
     if (category === "category1") {
         images = [
-            { src: "../image/anhcovn.jpg", title: "Ảnh 1 - Category 1", description: "Đây là mô tả của ảnh 1 trong Category 1", video: "https://www.youtube.com/watch?v=gMRmuHtVElg" },
-            { src: "../image/anhcovn.jpg", title: "Ảnh 2 - Category 1", description: "Đây là mô tả của ảnh 2 trong Category 1", video: "https://www.youtube.com/watch?v=6praEM_RDTc&t" },
-            { src: "../image/anhcovn.jpg", title: "Ảnh 3 - Category 1", description: "Đây là mô tả của ảnh 3 trong Category 1", video: "https://www.youtube.com/watch?v=mIQM9wrErBQ" },
-            { src: "../image/anhcovn.jpg", title: "Ảnh 4 - Category 1", description: "Đây là mô tả của ảnh 4 trong Category 1", video: "https://www.youtube.com/watch?v=819spKESC5w" },
-            { src: "../image/anhcovn.jpg", title: "Ảnh 5 - Category 1", description: "Đây là mô tả của ảnh 5 trong Category 1", video: "https://www.youtube.com/watch?v=oSoZwZ-AqnQ" },
-            { src: "../image/anhcovn.jpg", title: "Ảnh 5 - Category 1", description: "Đây là mô tả của ảnh 6 trong Category 1", video: "https://www.youtube.com/watch?v=RH7i8Fd__h4" }
+            { src: "../image/anhcovn.jpg", title: "Ảnh 1 - Category 1", description: "Đây là mô tả của ảnh 1 trong Category 1", video: "https://www.youtube.com/embed/gMRmuHtVElg" },
+            { src: "../image/anhcovn.jpg", title: "Ảnh 2 - Category 1", description: "Đây là mô tả của ảnh 2 trong Category 1", video: "https://www.youtube.com/embed/6praEM_RDTc" },
+            { src: "../image/anhcovn.jpg", title: "Ảnh 3 - Category 1", description: "Đây là mô tả của ảnh 3 trong Category 1", video: "https://www.youtube.com/embed/mIQM9wrErBQ" },
+            { src: "../image/anhcovn.jpg", title: "Ảnh 4 - Category 1", description: "Đây là mô tả của ảnh 4 trong Category 1", video: "https://www.youtube.com/embed/819spKESC5w" },
+            { src: "../image/anhcovn.jpg", title: "Ảnh 5 - Category 1", description: "Đây là mô tả của ảnh 5 trong Category 1", video: "https://www.youtube.com/embed/oSoZwZ-AqnQ" },
+            { src: "../image/anhcovn.jpg", title: "Ảnh 6 - Category 1", description: "Đây là mô tả của ảnh 6 trong Category 1", video: "https://www.youtube.com/embed/RH7i8Fd__h4" }
         ];
     } else if (category === "category2") {
         images = [
-            { src: "../image/chimhac.jpg", title: "Ảnh 1 - Category 2", description: "Đây là mô tả của ảnh 1 trong Category 2", video: "https://www.youtube.com/watch?v=gMRmuHtVElg" },
-            { src: "../image/chimhac.jpg", title: "Ảnh 2 - Category 2", description: "Đây là mô tả của ảnh 2 trong Category 2", video: "https://www.youtube.com/watch?v=gMRmuHtVElg" }
+            { src: "../image/chimhac.jpg", title: "Ảnh 1 - Category 2", description: "Đây là mô tả của ảnh 1 trong Category 2", video: "https://www.youtube.com/embed/gMRmuHtVElg" },
+            { src: "../image/chimhac.jpg", title: "Ảnh 2 - Category 2", description: "Đây là mô tả của ảnh 2 trong Category 2", video: "https://www.youtube.com/embed/gMRmuHtVElg" }
         ];
     }
-
+    
     images.forEach(image => {
         const imageContainer = document.createElement("div");
         imageContainer.classList.add("image-container");
@@ -53,8 +53,10 @@ function openPopup(videoSrc) {
 function closePopup() {
     const videoPopup = document.getElementById("videoPopup");
     const popupVideo = document.getElementById("popupVideo");
+    const imageDisplay = document.getElementById("imageDisplay");
 
-    popupVideo.pause(); // Dừng video khi đóng popup
+
     popupVideo.src = ""; // Xóa nguồn video
     videoPopup.style.display = "none"; // Ẩn popup
+    
 }
